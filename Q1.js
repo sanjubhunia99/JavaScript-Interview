91314fee5f815268575fec540d3e887dcfe10b1b
Sanju Bhunia <sanjubhunia93@gmail.com>
2026-05-07 20:24:15 +0530

    count frequency of each character

--- Q1.js
+++ Q1.js
@@ -1,1 +1,12 @@
-// 
+// Count Frequency of Each Character
+
+let string = "My name is Sanju Bhunia";
+let frequency = {};
+for (let index of string) {
+    if (frequency[index]) {
+        frequency[index]++;
+    } else {
+        frequency[index] = 1;
+    }
+}
+console.log(frequency);
