c34ec4d5944d5035ec51a99643d91aa9908206b3
Sanju Bhunia <sanjubhunia93@gmail.com>
2026-05-08 21:20:17 +0530

    EventLoop(Deep Understanding)

---
+++ Q4.js
@@ -0,0 +1,23 @@
+// EventLoop (Deep UnderStanding)
+
+console.log("Start");
+
+setTimeout(() => {
+    console.log("Timeout");
+    
+},0);
+Promise.resolve().then(() => {
+    console.log("Promise");
+    
+})
+console.log("End");
+
+
+/*
+Output: 
+
+Start
+End
+Promise
+Timeout
+*/
