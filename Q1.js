eb24e6d1102c95ada43f494df660a42d8ec6b7d1
Sanju Bhunia <sanjubhunia93@gmail.com>
2026-05-11 11:57:47 +0530

    Count Frequency of Each Character

--- Q1.js
+++ Q1.js
@@ -1,4 +1,19 @@
 // Count Frequency of Each Character
+/*Output:
+{
+  M: 1,
+  y: 1,
+  ' ': 4,
+  n: 3,
+  m: 1,
+  e: 1,
+  i: 2,
+  s: 1,
+  u: 2,
+  B: 1,
+  h: 1
+}
+*/
 
 let string = "My name is Sanju Bhunia";
 let frequency = {};
--- Q2.js
+++ Q2.js
@@ -1,13 +1,17 @@
 // first Non Reapeated Char ("aabbcddffe") --> "c"
 
-function firstNonRepetedChar(str) {
-    for (let char of str) {
-        if (str.indexOf(char) === str.lastIndexOf(char)) {
-            return char;
-        }
-    }
-return null;
-}
-console.log(firstNonRepetedChar("aabbcddffe"));
-
 // Output: c
+
+let str = "aabbcddffe";
+
+function firstNonRepetedChar(str) {
+  for (let char of str) {
+    if (str.indexOf(char) === str.lastIndexOf(char)) {
+      return char;
+    }
+  }
+
+  return null;
+}
+
+console.log(firstNonRepetedChar(str));
