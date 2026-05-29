a127d505463d09e9ea75b8b92f145171461314e5
Sanju Bhunia <sanjubhunia93@gmail.com>
2026-05-29 10:08:22 +0530

    Reverse Number

---
+++ Q11.js
@@ -0,0 +1,10 @@
+// Reverse Number
+// Input: 12345
+// Output: 54321
+
+function reverseNumber(num) {
+ 
+      return Number= num.toString().split("").reverse().join("");
+    
+}
+console.log(reverseNumber(12345)); // Output: 54321
